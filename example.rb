0beb03f62e4feaaefa1b35f340043dc8e0f97269
Natalie Eisner <natalieeisner5@gmail.com>
2018-07-10 15:25:29 +0000

    automatically backed up by learn

--- example.rb
+++ example.rb
@@ -1,10 +1,10 @@
 # $your_name = "NE"
 
-# def say_hello
-#   puts "Hello #{$your_name}"
-# end
+# # def say_hello
+# #   puts "Hello #{$your_name}"
+# # end
 
-say_hello
+# say_hello
 
 def say_hello(your_name)
   puts "Hello #{your_name}!"
