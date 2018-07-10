7234ebab4d9b34cb7e7158430a868e902aac0655
Natalie Eisner <natalieeisner5@gmail.com>
2018-07-10 15:54:32 +0000

    Done.

---
+++ example.rb
@@ -0,0 +1,14 @@
+# $your_name = "NE"
+
+# # def say_hello
+# #   puts "Hello #{$your_name}"
+# # end
+
+# say_hello
+
+def say_hello(your_name)
+  puts "Hello #{your_name}!"
+end
+
+
+say_hello ("Natalie")
